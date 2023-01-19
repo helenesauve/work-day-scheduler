4c6aeb1739b06ed95a8fc7c5aa6710de10e97c83
Hélène Sauvé <helenesauve@Helenes-MacBook-Pro.local>
2023-01-19 23:05:24 +0000

    changing time using moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,6 @@
-var currentDay = $('#currentDay');
+var now = moment();
+$("#currentDay").text(now.format("Do MMMM YYYY"));
 
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
 
-currentDay.text(today);
-
-today = dd + '/' + mm + '/' + yyyy;
-
-document.write(currentDay);
 
